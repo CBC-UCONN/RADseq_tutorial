53f469e5bf1818cb90cf2148bc3637dda9bd14e3
Noah Reid <noah.reid@uconn.edu>
2020-03-03 15:36:31 -0500

    refmap.pl

--- scripts/lacewings/c2_refmappl.sh
+++ scripts/lacewings/c2_refmappl.sh
@@ -1,13 +1,12 @@
 #!/bin/bash 
 #SBATCH --job-name=refmap.pl
-#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH --mail-user=
 #SBATCH --mail-type=ALL
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH -c 10
-#SBATCH --mem=30G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=15G
 #SBATCH --qos=general
 #SBATCH --partition=general
 
@@ -19,15 +18,13 @@ date
 module load stacks/2.41
 
 # input, output files, directories
-
 INDIR=../../results/aligned
+
 OUTDIR=../../results/stacks/refmap
 mkdir -p $OUTDIR
 
-# popmap file with .trim suffix
-POPMAP=../../metadata/popmap.txt
-cat $POPMAP | sed 's/\s/.trim\t/' >${POPMAP}.trim
-POPMAP=${POPMAP}.trim
+# popmap file
+POPMAP=../../metadata/lacewing_popmap.txt
 
 # refmap.pl -s option is broken. 
 ref_map.pl \
@@ -35,7 +32,11 @@ ref_map.pl \
 --popmap $POPMAP \
 -o $OUTDIR \
 -T 10 \
+-X "populations:-p 5" \
+-X "populations:-r 2" \
+-X "populations:--hwe" \
 -X "populations:--vcf" \
 -X "populations:--treemix" \
 -X "populations:--fasta-samples" \
 -X "populations:--fasta-loci"
+
